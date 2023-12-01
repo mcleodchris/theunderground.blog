373db0128b5b6398bf5f859fa7d88ef5ca4df1eb
Chris McLeod <chris@chrismcleod.dev>
2023-12-01 17:00:19 +0000

    remove heading anchors

--- config/plugins/markdown.js
+++ config/plugins/markdown.js
@@ -1,34 +1,25 @@
-const markdownIt = require('markdown-it');
-const markdownItPrism = require('markdown-it-prism');
-const markdownItAnchor = require('markdown-it-anchor');
-const markdownItClass = require('@toycode/markdown-it-class');
-const markdownItLinkAttributes = require('markdown-it-link-attributes');
-const markdownItEmoji = require('markdown-it-emoji');
-const markdownItFootnote = require('markdown-it-footnote');
-const markdownitMark = require('markdown-it-mark');
-const markdownitAbbr = require('markdown-it-abbr');
-const {slugifyString} = require('../utils');
+const markdownIt = require("markdown-it");
+const markdownItPrism = require("markdown-it-prism");
+const markdownItClass = require("@toycode/markdown-it-class");
+const markdownItLinkAttributes = require("markdown-it-link-attributes");
+const markdownItEmoji = require("markdown-it-emoji");
+const markdownItFootnote = require("markdown-it-footnote");
+const markdownitMark = require("markdown-it-mark");
+const markdownitAbbr = require("markdown-it-abbr");
 
 const markdownLib = markdownIt({
   html: true,
   breaks: true,
   linkify: true,
-  typographer: true
+  typographer: true,
 })
-  .disable('code')
+  .disable("code")
   .use(markdownItPrism, {
-    defaultLanguage: 'plaintext'
-  })
-  .use(markdownItAnchor, {
-    slugify: slugifyString,
-    tabIndex: false,
-    permalink: markdownItAnchor.permalink.headerLink({
-      class: 'heading-anchor'
-    })
+    defaultLanguage: "plaintext",
   })
   .use(markdownItClass, {
-    ol: 'list',
-    ul: 'list'
+    ol: "list",
+    ul: "list",
   })
   .use(markdownItLinkAttributes, [
     {
@@ -37,10 +28,10 @@ const markdownLib = markdownIt({
         return href.match(/^https?:\/\//);
       },
       attrs: {
-        target: '_blank',
-        rel: 'noopener'
-      }
-    }
+        target: "_blank",
+        rel: "noopener",
+      },
+    },
   ])
   .use(markdownItEmoji)
   .use(markdownItFootnote)
